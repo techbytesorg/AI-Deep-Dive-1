cf6e54d688ec5aa49b1cc4a4645da39b6928a265
Zony Yu <zony249@gmail.com>
2021-08-18 09:51:06 -0600

    made workshop branch

--- workshop-1-linear-regression/gpu-perf.py
+++ workshop-1-linear-regression/gpu-perf.py
@@ -1,14 +1,6 @@
 from email import message
 from flask import Flask, render_template, redirect, url_for, request, jsonify
 
-# Import these modules
-import numpy as np
-import torch
-from sklearn.preprocessing import PolynomialFeatures
-
-
-gpu_model = torch.jit.load("gpu-predictor.pt")
-gpu_model.eval() # set to eval mode
 
 
 app = Flask(__name__)
@@ -27,18 +19,16 @@ def test1():
     mem = request.form.get("mem") # Memory Size
 
     # create inputs (IN THIS ORDER OF trans, area, mem, pnode)
-    inputs = np.array([trans, area, mem, pnode]).reshape(1, 4)
-    poly = PolynomialFeatures(degree=3) 
-    inputs_p = poly.fit_transform(inputs)
+
 
     # convert to torch.tensor
-    torch_input = torch.from_numpy(inputs_p).float()
+
 
     # prediction
-    gflops = gpu_model(torch_input)
+
     
     #.item() gets the value in the torch.tensor
-    return jsonify({'pred' : "GFLOPS: " + str(gflops.item())})
+    return jsonify({'pred' : "GFLOPS: " + str(None)})
 
 if __name__ == "__main__":
     app.run(debug=True)
--- workshop-2-logistic-regression/suv.py
+++ workshop-2-logistic-regression/suv.py
@@ -2,18 +2,14 @@ from flask import Flask, render_template, jsonify, request
 import requests
 
 # import all of these modules
-import numpy as np
-import torch
-from sklearn.preprocessing import PolynomialFeatures
 
 
 
-model = torch.jit.load('suv_predictor.pt')
-model.eval() # set it to eval mode
+
 
 # We need to convert 3 input features into the 20 polynomial features
 # that the model can accept
-poly = PolynomialFeatures(degree=3) 
+
 
 
 app = Flask(__name__)
@@ -32,47 +28,27 @@ def get_weather():
     salary = request.args.get("salary")
 
     # Convert gender to lower case, and handle invalid inputs
-    gender = gender.lower()
-    if gender == 'male':
-        gender = 0
-    elif gender == 'female':
-        gender = 1
-    elif gender == 'other':
-        gender = 1
-    else:
-        return jsonify({"code": 500, "msg": "Invalid Gender"})
+
 
 
     # Age must be integer
-    try:
-        age = int(age)
-    except ValueError:
-        return jsonify({"code": 500, "msg": "Invalid Age"})
+
 
     
     # Salary must be integer
-    try:
-        salary = int(salary)
-    except ValueError:
-        return jsonify({"code": 500, "msg": "Invalid salary"})
+
 
 
     # form input variables into a 1x20 torch.tensor
-    inputs_raw = np.array([[age, salary, gender]])
-    inputs_poly = poly.fit_transform(inputs_raw)
-    inputs = torch.from_numpy(inputs_poly).float()
+
 
     # make prediction
-    pred = model(inputs).item() #.item() gets the value from the tensor
-    result = "YES"
-    if pred < 0.5:
-        result = "NO"
 
-    print(result)
-    
+
+ 
 
    
-    return jsonify({"code": 200, "msg": result})
+    return jsonify({"code": 200, "msg": None})
 
 
 if __name__ == "__main__":
